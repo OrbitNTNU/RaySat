128aeb98661dda9452dcf78b3bc894a1affbcd8c
Michael Birkeland <tpdm@icloud.com>
2024-10-06 15:48:13 +0200

    feat: Add pressure reading

---
+++ include/sensors/pressure.h
@@ -0,0 +1,2 @@
+void setup_pressure();
+double read_pressure();
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "sensors/pressure.h"
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Initializing...");
+  // Wire.begin();
   int result = myFunction(2, 3);
+  setup_pressure();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  double pressure = read_pressure();
+  Serial.print("Pressure: ");
+  Serial.println(pressure);
+  delay(1000);
 }
 
 // put function definitions here:
