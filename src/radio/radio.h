cf3c07c80ed0e64c8233bd5191fa2c0c99a19306
Michael Birkeland <tpdm@icloud.com>
2025-02-23 13:21:16 +0100

    chore: radio changes

--- src/radio/radio.h
+++ src/radio/radio.h
@@ -1,26 +1,46 @@
 #include <SoftwareSerial.h>
 #include <vector>
+#pragma once
 
-enum class RadioMode{unknown, setting, transmit};   // Modes that the radio can be in
+enum class RadioMode
+{
+    unknown,
+    setting,
+    transmit
+}; // Modes that the radio can be in
 
-class Radio{
-    HardwareSerial radioSerial{PC5, PC4};  // Uart pins to radio (D1=TX/D0=RX on CN9)
-    RadioMode mode = RadioMode::unknown;   // Makes the mode unknown
-    void enterSettingMode();           // Entering settings mode
-    void enterTransmitMode();   // Entering communicate mode
-    // void sendSetup();   // Sending settings/setup commands
+class Radio
+{
+
+
+public:
+    std::pair<int, String> Radio::setup(int aprsInterval = 20, bool _verbose = true);
+    void loop();
+
+    String read(); // Reading from radio
+
+
+private:
+    HardwareSerial radioSerial{PC5, PC4}; // Uart pins to radio (D1=TX/D0=RX on CN9)
+    RadioMode mode = RadioMode::unknown;  // Initial mode is unknown
+    unsigned long previousMillis = 0;
+    const long interval = 10000;
+
+    void setMode(RadioMode desiredMode);
+    void enterSettingMode();
+    std::pair<int, String> enterTransmitMode();
+    String getModeString(RadioMode mode);
+
+    void sendSetup();
+    std::pair<int, String> sendSetupCommand(String command);
+    std::pair<int, String> sendConfiguration(std::vector<String> commandsToSend);
     void sendMessage(String message);
     void sendCtrlC();
-    void setMode(RadioMode desiredMode);
-    String getModeString(RadioMode mode);
-    void menu();
-    void write(const String& message);
 
-    unsigned long previousMillis = 0; 
-    const long interval = 10000;
-public: 
-    void setup();   
-    void loop();
-   
-    String readFromRadio();     // Reading from radio
+    void write(const String &message);
+    void transmit(String message);
+    String read();
+
+    void menu();
+    bool verbose;
 };
